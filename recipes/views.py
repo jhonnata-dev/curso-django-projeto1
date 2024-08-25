7ffdcc4bcfa1c73b4ff19326e2b190a15f3744e6
Jhonnata D. Martins <jhonnatadmartins@gmail.com>
2024-08-24 22:57:59 -0300

    Exemplo de template temp

--- recipes/views.py
+++ recipes/views.py
@@ -2,10 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('home 1')
+    return render(request, 'recipes/home.html', context= {
+        'name': 'Luiz Otavio',
+    })
+
 
 def sobre(request):
     return HttpResponse('Sobre')
 
 def contato(request):
-    return HttpResponse('Contato')
+    return render(request, 'me-apague/temp.html')
