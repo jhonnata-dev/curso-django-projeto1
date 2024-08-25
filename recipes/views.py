618e5a0cdf8a70531088509e255272e6cdb14831
Jhonnata D. Martins <jhonnatadmartins@gmail.com>
2024-08-25 20:00:23 -0300

    create deetails

--- recipes/views.py
+++ recipes/views.py
@@ -4,3 +4,8 @@ def home(request):
     return render(request, 'recipes/pages/home.html', context= {
         'name': 'Luiz Otavio',
     })
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', context= {
+        'name': 'Luiz Otavio',
+    })
