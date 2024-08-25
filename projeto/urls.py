4038c0f6af6039bd61126fd4a2c7702234fe0d43
Jhonnata D. Martins <jhonnatadmartins@gmail.com>
2024-08-24 21:53:55 -0300

    adding urls and views

--- projeto/urls.py
+++ projeto/urls.py
@@ -15,25 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
-
-# User: HTTP REQUESTE <- HTTP RESPONSE :Server
-
-# HTTP REQUEST
-def home(request):
-    return HttpResponse('home')
-    # return HTTP Response
-
-def sobre(request):
-    return HttpResponse('Sobre')
-
-def contato(request):
-    return HttpResponse('Contato')
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', home), #home
-    path('sobre/', sobre), #sobre
-    path('contato/', contato), #contato
+    path('', include('recipes.urls'))
 ]
