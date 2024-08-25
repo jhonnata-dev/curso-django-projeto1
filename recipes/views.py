d53571f5a0aa0b3dacfee25b4427a259f026b83c
Jhonnata D. Martins <jhonnatadmartins@gmail.com>
2024-08-25 11:50:28 -0300

    removed contato and sobre

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def home(request):
     return render(request, 'recipes/home.html', context= {
         'name': 'Luiz Otavio',
     })
-
-
-def sobre(request):
-    return HttpResponse('Sobre')
-
-def contato(request):
-    return render(request, 'me-apague/temp.html')
