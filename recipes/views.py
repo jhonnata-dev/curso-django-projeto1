c3a32dc0173e0a8659c4966b1885d1cb6f2cd7af
Jhonnata D. Martins <jhonnatadmartins@gmail.com>
2024-08-26 22:47:07 -0300

    update pages

--- recipes/views.py
+++ recipes/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
+from utils.recipes.factory import make_recipe
 
 def home(request):
     return render(request, 'recipes/pages/home.html', context= {
-        'name': 'Luiz Otavio',
+        'recipes': [make_recipe() for _ in range(10)],
     })
 
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context= {
-        'name': 'Luiz Otavio',
+        'recipe': make_recipe(),
+        'is_detail_page': True,
     })
